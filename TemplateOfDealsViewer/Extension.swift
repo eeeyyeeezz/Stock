b30ba8caffe651a5b131920771d19aa281618fad
eximmlich@gmail.com <eximmlich@gmail.com>
2023-05-25 19:28:11 +0700

    hotfix

---
+++ TemplateOfDealsViewer/Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.swift
+//  TemplateOfDealsViewer
+//
+//  Created by Даниил Назаров on 25.05.2023.
+//
+
+import Foundation
+
+extension Double {
+	func rounded(toPlaces places: Int) -> Double {
+		let divisor = pow(10.0, Double(places))
+		return (self * divisor).rounded() / divisor
+	}
+}
